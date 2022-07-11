3bf6f88cf6165b2fe1f7f25d14a70a14184b4efc
Mason Stuart <mason.stuart05@outlook.com>
2022-07-10 17:25:38 -0700

    fixed turret thinhg

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/subsystem/TurretSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/subsystem/TurretSubsystem.java
@@ -1,7 +1,14 @@
 package org.firstinspires.ftc.teamcode.common.commandbase.subsystem;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class TurretSubsystem extends SubsystemBase {
+    private final Servo left;
+    private final Servo right;
 
+    public TurretSubsystem(Servo left, Servo right) {
+        this.left = left;
+        this.right = right;
+    }
 }
