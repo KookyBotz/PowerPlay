0da6fa2b797f6f00993a933d8e9571e13faf7c01
ProDCG <mason.stuart05@outlook.com>
2022-07-28 12:44:07 -0700

    Changed drivetrain to abstract class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/purepursuit/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/purepursuit/Drivetrain.java
@@ -1,4 +1,11 @@
 package org.firstinspires.ftc.teamcode.common.purepursuit;
 
-public class Drivetrain {
+public abstract class Drivetrain {
+    private double x, y, t;
+
+    void set(double x, double y, double t) {
+        this.x = x;
+        this.y = y;
+        this.t = t;
+    }
 }
