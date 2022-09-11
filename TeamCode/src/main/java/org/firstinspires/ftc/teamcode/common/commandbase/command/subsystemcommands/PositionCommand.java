f38b5e0c99f7d9a7420796817d703425408074a1
Mason Stuart <mason.stuart05@outlook.com>
2022-09-11 16:11:55 -0700

    scuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/PositionCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/PositionCommand.java
@@ -5,8 +5,10 @@ import static org.firstinspires.ftc.teamcode.common.purepursuit.path.PurePursuit
 import static org.firstinspires.ftc.teamcode.common.purepursuit.path.PurePursuitConfig.pCoefficientY;
 
 import com.arcrobotics.ftclib.command.CommandBase;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.common.purepursuit.drive.Drivetrain;
+import org.firstinspires.ftc.teamcode.common.purepursuit.geometry.MotionProfile;
 import org.firstinspires.ftc.teamcode.common.purepursuit.geometry.Pose;
 import org.firstinspires.ftc.teamcode.common.purepursuit.localizer.Localizer;
 import org.firstinspires.ftc.teamcode.common.purepursuit.path.PurePursuitConfig;
@@ -16,21 +18,30 @@ public class PositionCommand extends CommandBase {
     Drivetrain drivetrain;
     Localizer localizer;
     Pose targetPose;
+    MotionProfile profile;
+    ElapsedTime timer;
 
-    public PositionCommand(Drivetrain drivetrain, Localizer localizer, Pose targetPose) {
+    public PositionCommand(Drivetrain drivetrain, Localizer localizer, Pose targetPose, MotionProfile profile) {
         this.drivetrain = drivetrain;
         this.localizer = localizer;
         this.targetPose = targetPose;
+        this.profile = profile;
     }
 
     @Override
     public void execute() {
-        drivetrain.set(PurePursuitController.goToPosition(localizer.getPos(), targetPose, new Pose(pCoefficientX, pCoefficientY, pCoefficientH)));
+        if (timer == null) {
+            timer = new ElapsedTime();
+        }
+        drivetrain.set(PurePursuitController.goToPosition(localizer.getPos(), targetPose, new Pose(pCoefficientX, pCoefficientY, pCoefficientH)), profile.update(timer.milliseconds()));
     }
 
     @Override
     public boolean isFinished() {
-        return (Math.abs(localizer.getPos().distanceTo(targetPose)) < PurePursuitConfig.ALLOWED_TRANSLATIONAL_ERROR && targetPose.subtract(localizer.getPos()).heading < PurePursuitConfig.ALLOWED_HEADING_ERROR);
+        boolean a = (Math.abs(localizer.getPos().distanceTo(targetPose)) < PurePursuitConfig.ALLOWED_TRANSLATIONAL_ERROR
+                && targetPose.subtract(localizer.getPos()).heading < PurePursuitConfig.ALLOWED_HEADING_ERROR);
+        System.out.print(a);
+        return a;
     }
 
     @Override
