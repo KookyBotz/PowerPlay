63bc2a7e08abee5e76c7792d7ca250ced1c1219b
Mason Stuart <mason.stuart05@outlook.com>
2022-09-18 10:41:34 -0700

    Commmiting prior to use of eocv sim

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/powerplay/SleeveDetection.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.common.powerplay;
+
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class SleeveDetection extends OpenCvPipeline {
+    public enum SleeveRotation {
+        RED,
+        GREEN,
+        BLUE
+    }
+
+    @Override
+    public Mat processFrame(Mat input) {
+
+    }
+}
