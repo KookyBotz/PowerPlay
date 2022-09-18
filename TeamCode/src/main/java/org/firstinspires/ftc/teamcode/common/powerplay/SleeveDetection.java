b977958acfaaa8a623732800166894bd76c75384
Mason Stuart <mason.stuart05@outlook.com>
2022-09-18 11:29:14 -0700

    pipeline test for yellow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/powerplay/SleeveDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/powerplay/SleeveDetection.java
@@ -1,6 +1,10 @@
 package org.firstinspires.ftc.teamcode.common.powerplay;
 
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 public class SleeveDetection extends OpenCvPipeline {
@@ -10,8 +14,28 @@ public class SleeveDetection extends OpenCvPipeline {
         BLUE
     }
 
+    Scalar  lower_yellow_bounds = new Scalar(180, 180, 0),
+            upper_yellow_bounds = new Scalar(255, 255, 30);
+
+    Mat thresh = new Mat();
+
+    public static Point SLEEVE_TOPLEFT_ANCHOR_POINT = new Point(30, 23);
+    public static int REGION_WIDTH = 30;
+    public static int REGION_HEIGHT = 50;
+
+    Point sleeve_pointA = new Point(
+            SLEEVE_TOPLEFT_ANCHOR_POINT.x,
+            SLEEVE_TOPLEFT_ANCHOR_POINT.y);
+
+    Point sleeve_pointB = new Point(
+            SLEEVE_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
+            SLEEVE_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+
+    private volatile SleeveRotation rotation = SleeveRotation.RED;
+
     @Override
     public Mat processFrame(Mat input) {
-
+        Core.inRange(input, lower_yellow_bounds, upper_yellow_bounds, thresh);
+        return thresh;
     }
 }
