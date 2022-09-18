9b5b3d6f431ec956cee6c87efc994aa5db5b9fdd
Mason Stuart <mason.stuart05@outlook.com>
2022-09-18 14:03:10 -0700

    Percent checking

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/powerplay/RelocalizerDetection.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.common.powerplay;
+
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class RelocalizerDetection extends OpenCvPipeline {
+    @Override
+    public Mat processFrame(Mat input) {
+        return input;
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/powerplay/SleeveDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/powerplay/SleeveDetection.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.common.powerplay;
 
-import android.os.DropBoxManager;
-
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
@@ -9,8 +7,6 @@ import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class SleeveDetection extends OpenCvPipeline {
     /*
@@ -25,20 +21,29 @@ public class SleeveDetection extends OpenCvPipeline {
         MAGENTA
     }
 
-    public static Scalar lower_yellow_bounds  = new Scalar(205, 205, 0, 255),
-                         upper_yellow_bounds  = new Scalar(255, 255, 49, 255),
-                         lower_cyan_bounds    = new Scalar(0, 205, 205, 255),
-                         upper_cyan_bounds    = new Scalar(49, 255, 255, 255),
-                         lower_magenta_bounds = new Scalar(205, 0, 205, 255),
-                         upper_magenta_bounds = new Scalar(255, 49, 255, 255);
-
-    public static Point SLEEVE_TOPLEFT_ANCHOR_POINT = new Point(30, 23);
+    public static Point SLEEVE_TOPLEFT_ANCHOR_POINT = new Point(145, 168);
 
     public static int REGION_WIDTH = 30;
     public static int REGION_HEIGHT = 50;
 
     public static final int COLOR_MAX = 49;
     public static final int COLOR_MIN = 205;
+
+    private double yelPercent, cyaPercent, magPercent;
+    
+    private static final Scalar
+            lower_yellow_bounds  = new Scalar(COLOR_MIN, COLOR_MIN, 0, 255),
+            upper_yellow_bounds  = new Scalar(255, 255, COLOR_MAX, 255),
+            lower_cyan_bounds    = new Scalar(0, COLOR_MIN, COLOR_MIN, 255),
+            upper_cyan_bounds    = new Scalar(COLOR_MAX, 255, 255, 255),
+            lower_magenta_bounds = new Scalar(COLOR_MIN, 0, COLOR_MIN, 255),
+            upper_magenta_bounds = new Scalar(255, COLOR_MAX, 255, 255);
+
+    private final Scalar
+            RED   = new Scalar(255, 0, 0),
+            GREEN = new Scalar(0, 255, 0),
+            BLUE  = new Scalar(0, 0, 255),
+            WHITE = new Scalar(255, 255, 255);
 
     private Mat yelMat = new Mat(), cyaMat = new Mat(), magMat = new Mat();
 
@@ -52,17 +57,25 @@ public class SleeveDetection extends OpenCvPipeline {
 
     private volatile SleeveRotation rotation = SleeveRotation.YELLOW;
 
-    Telemetry telemetry;
-
-    public SleeveDetection(Telemetry telemetry) {
-        this.telemetry = telemetry;
-    }
     @Override
     public Mat processFrame(Mat input) {
         Imgproc.blur(input, input, new Size(5, 5));
+
         Core.inRange(input, lower_yellow_bounds, upper_yellow_bounds, yelMat);
-        telemetry.addData("val:", Core.countNonZero(yelMat));
-        telemetry.update();
+        Core.inRange(input, lower_cyan_bounds, upper_cyan_bounds, cyaMat);
+        Core.inRange(input, lower_magenta_bounds, upper_magenta_bounds, magMat);
+
+        yelPercent = Core.countNonZero(yelMat);
+        cyaPercent = Core.countNonZero(cyaMat);
+        magPercent = Core.countNonZero(magMat);
+
+        Imgproc.rectangle(
+                yelMat,
+                sleeve_pointA,
+                sleeve_pointB,
+                GREEN,
+                2
+        );
         return yelMat;
     }
 }
