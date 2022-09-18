50144fc1a227e9ba260b17e31e0e01b9f256445f
Mason Stuart <mason.stuart05@outlook.com>
2022-09-18 11:51:19 -0700

    cleaned up file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/powerplay/SleeveDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/powerplay/SleeveDetection.java
@@ -4,6 +4,7 @@ import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
@@ -14,14 +15,12 @@ public class SleeveDetection extends OpenCvPipeline {
         BLUE
     }
 
-    public static Scalar  lower_yellow_bounds = new Scalar(205, 205, 0, 255),
-            upper_yellow_bounds = new Scalar(255, 255, 49, 255);
-    public static Scalar  lower_cyan_bounds = new Scalar(0, 205, 205, 255),
-            upper_cyan_bounds = new Scalar(49, 255, 255, 255);
-    public static Scalar  lower_yellow_bounds = new Scalar(205, 0, 205, 255),
-            upper_yellow_bounds = new Scalar(255, 49, 255, 255);
-
-    Mat thresh = new Mat();
+    public static Scalar lower_yellow_bounds =  new Scalar(205, 205, 0, 255),
+                         upper_yellow_bounds =  new Scalar(255, 255, 49, 255),
+                         lower_cyan_bounds =    new Scalar(0, 205, 205, 255),
+                         upper_cyan_bounds =    new Scalar(49, 255, 255, 255),
+                         lower_magenta_bounds = new Scalar(205, 0, 205, 255),
+                         upper_magenta_bounds = new Scalar(255, 49, 255, 255);
 
     public static Point SLEEVE_TOPLEFT_ANCHOR_POINT = new Point(30, 23);
     public static int REGION_WIDTH = 30;
@@ -41,7 +40,8 @@ public class SleeveDetection extends OpenCvPipeline {
     public Mat processFrame(Mat input) {
 //        Core.inRange(input, lower_yellow_bounds, upper_yellow_bounds, thresh);
 //        return thresh;
-        Core.inRange(input, lower_cyan_bounds, upper_cyan_bounds, input);
+        Imgproc.blur(input, input, new Size(5, 5));
+        Core.inRange(input, lower_magenta_bounds, upper_magenta_bounds, input);
         return input;
     }
 }
