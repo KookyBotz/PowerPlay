96d163da603b77e6f36123b6f6ca0680b98f6e0e
Mason Stuart <mason.stuart05@outlook.com>
2022-09-24 08:53:51 -0700

    bad

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/powerplay/SleeveDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/powerplay/SleeveDetection.java
@@ -1,8 +1,5 @@
 package org.firstinspires.ftc.teamcode.common.powerplay;
 
-import static org.firstinspires.ftc.teamcode.common.powerplay.VisionConstants.REGION_HEIGHT;
-import static org.firstinspires.ftc.teamcode.common.powerplay.VisionConstants.REGION_WIDTH;
-
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
@@ -11,7 +8,6 @@ import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
-import org.firstinspires.ftc.teamcode.common.powerplay.VisionConstants.*;
 
 public class SleeveDetection extends OpenCvPipeline {
     /*
@@ -25,6 +21,28 @@ public class SleeveDetection extends OpenCvPipeline {
         CYAN,
         MAGENTA
     }
+
+    private static Point SLEEVE_TOPLEFT_ANCHOR_POINT = new Point(145, 168);
+
+    public static int REGION_WIDTH = 30;
+    public static int REGION_HEIGHT = 50;
+
+    public static int COLOR_MAX = 49;
+    public static int COLOR_MIN = 205;
+
+    private static final Scalar
+            lower_yellow_bounds  = new Scalar(COLOR_MIN, COLOR_MIN, 0, 255),
+            upper_yellow_bounds  = new Scalar(255, 255, COLOR_MAX, 255),
+            lower_cyan_bounds    = new Scalar(0, COLOR_MIN, COLOR_MIN, 255),
+            upper_cyan_bounds    = new Scalar(COLOR_MAX, 255, 255, 255),
+            lower_magenta_bounds = new Scalar(COLOR_MIN, 0, COLOR_MIN, 255),
+            upper_magenta_bounds = new Scalar(255, COLOR_MAX, 255, 255);
+
+    private final Scalar
+            YELLOW  = new Scalar(255, 255, 0),
+            CYAN    = new Scalar(0, 255, 255),
+            MAGENTA = new Scalar(255, 0, 255),
+            WHITE   = new Scalar(255, 255, 255);
 
     private double yelPercent, cyaPercent, magPercent;
 
