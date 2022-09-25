f7e2c7ccc17ceb1d8f411e973ddf631792db3b44
Mason Stuart <mason.stuart05@outlook.com>
2022-09-24 20:51:04 -0700

    tapedetection

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/powerplay/TapeDetection.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.common.powerplay;
+
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class TapeDetection extends OpenCvPipeline {
+    public TapeDetection() {
+
+    }
+
+    @Override
+    public Mat processFrame(Mat input) {
+        return null;
+    }
+}
