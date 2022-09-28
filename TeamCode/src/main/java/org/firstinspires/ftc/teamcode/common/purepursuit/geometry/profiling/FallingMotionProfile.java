ebbe174d8158ff20991bc0b63c56706fd0a34398
Mason Stuart <mason.stuart05@outlook.com>
2022-09-27 23:39:41 -0700

    base implementation for falling motion profile (opposite of rising)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/purepursuit/geometry/profiling/FallingMotionProfile.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.common.purepursuit.geometry.profiling;
+
+public class FallingMotionProfile implements MotionProfile {
+    private final double maxV;
+    private final double maxA;
+
+    public FallingMotionProfile(double maxV, double maxA) {
+        this.maxV = maxV;
+        this.maxA = maxA;
+    }
+
+    @Override
+    public double update(double time) {
+        return 0;
+    }
+}
