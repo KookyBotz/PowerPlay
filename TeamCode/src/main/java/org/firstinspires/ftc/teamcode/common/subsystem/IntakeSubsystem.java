17db5dfb2324c50d9658df87660f500053fbf97f
Mason Stuart <mason.stuart05@outlook.com>
2022-09-28 08:50:13 -0700

    extension motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/IntakeSubsystem.java
@@ -1,17 +1,20 @@
 package org.firstinspires.ftc.teamcode.common.subsystem;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class IntakeSubsystem extends SubsystemBase {
+    private final MotorEx extension;
     private final Servo barLeft, barRight;
 
-    public IntakeSubsystem(Servo barLeft, Servo barRight) {
+    public IntakeSubsystem(MotorEx extension, Servo barLeft, Servo barRight) {
+        this.extension = extension;
         this.barLeft = barLeft;
         this.barRight = barRight;
     }
 
-    public void setPos(double pos) {
-
+    public void setExtension(int pos) {
+        extension.setTargetPosition(pos);
     }
 }
