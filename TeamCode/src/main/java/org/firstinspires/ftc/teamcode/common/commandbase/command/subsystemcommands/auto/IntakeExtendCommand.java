0182b059208594d3dd30cdccce9f3d8d0e927ec6
Mason Stuart <mason.stuart05@outlook.com>
2022-10-01 00:10:54 -0700

    nested extend intake command

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/auto/IntakeExtendCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/auto/IntakeExtendCommand.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.common.commandbase.command.subsystemcommands.auto;
 
+import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.arcrobotics.ftclib.command.WaitUntilCommand;
 
@@ -8,7 +9,10 @@ import org.firstinspires.ftc.teamcode.common.hardware.Robot;
 public class IntakeExtendCommand extends SequentialCommandGroup {
     public IntakeExtendCommand(Robot robot) {
         super(
-                new WaitUntilCommand(() -> robot.lift.getPos() < robot.lift.high_pos - 10)
+                new WaitUntilCommand(() -> robot.lift.getPos() < robot.lift.high_pos - 10),
+                new InstantCommand(() -> robot.intake.extensionOut())
+                .alongWith(new InstantCommand(() -> robot.intake.openClaw()))
+                .alongWith(new InstantCommand(() -> robot.intake.extendForebar()))
         );
     }
 }
