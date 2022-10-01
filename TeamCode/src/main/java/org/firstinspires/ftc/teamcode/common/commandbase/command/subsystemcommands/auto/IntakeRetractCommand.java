820c736a8a63ce9dbcdfd675fd03297ab947eb31
Mason Stuart <mason.stuart05@outlook.com>
2022-10-01 00:39:09 -0700

    claw command

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/auto/IntakeRetractCommand.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.common.commandbase.command.subsystemcommands.auto;
+
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+
+import org.firstinspires.ftc.teamcode.common.hardware.Robot;
+
+public class IntakeRetractCommand extends SequentialCommandGroup {
+    public IntakeRetractCommand(Robot robot) {
+        super(
+
+        );
+    }
+}
