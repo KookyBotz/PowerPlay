60163d492c698ce75ccc23a2cb6f310942d1c084
Mason Stuart <mason.stuart05@outlook.com>
2022-09-30 23:55:54 -0700

    lift extend command change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/auto/LiftExtendCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/auto/LiftExtendCommand.java
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.teamcode.common.commandbase.command.subsystemcommands.auto;
 
+import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 
 import org.firstinspires.ftc.teamcode.common.hardware.Robot;
 
 public class LiftExtendCommand extends SequentialCommandGroup {
     public LiftExtendCommand(Robot robot) {
-        robot.lift.setPos(robot.lift.high_pos);
+        super(
+                new InstantCommand(() -> robot.lift.setPos(robot.lift.high_pos))
+        );
     }
 }
