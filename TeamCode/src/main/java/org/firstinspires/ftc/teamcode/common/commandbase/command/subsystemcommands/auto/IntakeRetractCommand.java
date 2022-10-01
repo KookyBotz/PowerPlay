bbd9bbb840ce6ea2e1f40a20bb25a460f7c8e7ca
Mason Stuart <mason.stuart05@outlook.com>
2022-10-01 10:11:55 -0700

    changing retract command

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/auto/IntakeRetractCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/auto/IntakeRetractCommand.java
@@ -2,13 +2,15 @@ package org.firstinspires.ftc.teamcode.common.commandbase.command.subsystemcomma
 
 import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitUntilCommand;
 
 import org.firstinspires.ftc.teamcode.common.hardware.Robot;
 
 public class IntakeRetractCommand extends SequentialCommandGroup {
     public IntakeRetractCommand(Robot robot) {
         super(
-            new InstantCommand(() -> robot.intake.extensionIn())
+                new InstantCommand(() -> robot.intake.extensionIn()),
+                new WaitUntilCommand(() -> robot.intake)
         );
     }
 }
