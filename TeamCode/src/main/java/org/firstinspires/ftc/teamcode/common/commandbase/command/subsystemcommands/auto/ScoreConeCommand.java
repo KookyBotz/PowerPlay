a492bd6790a81d3cd539878a28106cd4aede5b22
Mason Stuart <mason.stuart05@outlook.com>
2022-10-01 18:05:52 -0700

    accidentally modified wrong file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/auto/ScoreConeCommand.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.common.commandbase.command.subsystemcommands.auto;
+
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+
+public class ScoreConeCommand extends SequentialCommandGroup {
+    public ScoreConeCommand
+}
