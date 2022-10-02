d78d31a3ba46e4d042f4dcd7d6498bf07468edf9
Mason Stuart <mason.stuart05@outlook.com>
2022-10-01 18:07:53 -0700

    score cone command for auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/auto/ScoreConeCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/auto/ScoreConeCommand.java
@@ -1,7 +1,21 @@
 package org.firstinspires.ftc.teamcode.common.commandbase.command.subsystemcommands.auto;
 
+import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+import com.arcrobotics.ftclib.command.WaitUntilCommand;
+
+import org.firstinspires.ftc.teamcode.common.commandbase.command.subsystemcommands.LiftExtendCommand;
+import org.firstinspires.ftc.teamcode.common.commandbase.command.subsystemcommands.LiftRetractCommand;
+import org.firstinspires.ftc.teamcode.common.hardware.Robot;
 
 public class ScoreConeCommand extends SequentialCommandGroup {
-    public ScoreConeCommand
+    public ScoreConeCommand(Robot robot) {
+        super(
+                new LiftExtendCommand(robot),
+                new WaitUntilCommand(() -> robot.lift.getPos() == robot.lift.high_pos),
+                new WaitCommand(300),
+                new LiftRetractCommand(robot)
+        );
+    }
 }
