81af8e34f8880ac9ca5253df4da7ae8ad1cb9f44
Mason Stuart <mason.stuart05@outlook.com>
2022-10-01 21:28:17 -0700

    modified extend

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/IntakeExtendCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/IntakeExtendCommand.java
@@ -9,10 +9,9 @@ import org.firstinspires.ftc.teamcode.common.hardware.Robot;
 import org.firstinspires.ftc.teamcode.common.purepursuit.geometry.Pose;
 
 public class IntakeExtendCommand extends SequentialCommandGroup {
-    public IntakeExtendCommand(Robot robot) {
+    public IntakeExtendCommand(Robot robot, Pose pose) {
         super(
                 new WaitUntilCommand(() -> robot.lift.getPos() < robot.lift.high_pos - 10),
-//                new PositionCommand(robot.drivetrain, robot.localizer, pose),
                 new InstantCommand(() -> robot.intake.extensionOut())
                 .alongWith(new InstantCommand(() -> robot.intake.openClaw()))
                 .alongWith(new InstantCommand(() -> robot.intake.extendForebar()))
