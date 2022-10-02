58426d27de7743c631bbe41800619fcb32e1e5e4
Mason Stuart <mason.stuart05@outlook.com>
2022-10-01 23:12:03 -0700

    screw comments lol

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/IntakeRetractCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/IntakeRetractCommand.java
@@ -10,7 +10,6 @@ public class IntakeRetractCommand extends SequentialCommandGroup {
     public IntakeRetractCommand(Robot robot) {
         super(
                 new InstantCommand(() -> robot.intake.closeForebar()),
-                // wait until forebar is halfway
                 new InstantCommand(() -> robot.intake.extensionIn()),
                 new WaitUntilCommand(() -> robot.intake.getExtension() < robot.intake.extension_out_pos - 10)
         );
