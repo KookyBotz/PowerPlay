a6c565ff3f07aadc247578c56a8480c21525a60d
Mason Stuart <mason.stuart05@outlook.com>
2022-10-02 23:12:57 -0700

    formatting and removed constructor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/powerplay/TapeDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/powerplay/TapeDetection.java
@@ -5,18 +5,15 @@ import org.opencv.core.Scalar;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 public class TapeDetection extends OpenCvPipeline {
-    public TapeDetection() {
-
-    }
 
     private static final Scalar
-            lower_red_bounds = new Scalar(200, 0, 0),
-            upper_red_bounds = new Scalar(255, 100, 100),
+            lower_red_bounds  = new Scalar(200, 0, 0),
+            upper_red_bounds  = new Scalar(255, 100, 100),
             lower_blue_bounds = new Scalar(0, 0, 200),
             upper_blue_bounds = new Scalar(100, 100, 255);
 
     private final Scalar
-            RED = new Scalar(255, 0, 0),
+            RED  = new Scalar(255, 0, 0),
             BLUE = new Scalar(0, 0, 255);
 
     @Override
