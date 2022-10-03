17710a30163501988f9555193c9e29bcc1373f69
Mason Stuart <mason.stuart05@outlook.com>
2022-10-02 23:11:11 -0700

    red bounds

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/powerplay/TapeDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/powerplay/TapeDetection.java
@@ -1,12 +1,17 @@
 package org.firstinspires.ftc.teamcode.common.powerplay;
 
 import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 public class TapeDetection extends OpenCvPipeline {
     public TapeDetection() {
 
     }
+
+    private static final Scalar
+            lower_red_bounds = new Scalar(200, 0, 0),
+            upper_red_bounds = new Scalar(255, 100, 100);
 
     @Override
     public Mat processFrame(Mat input) {
