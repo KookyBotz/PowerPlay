a644d9c1df29059623ab79e5ca20bb331a02a387
Mason Stuart <mason.stuart05@outlook.com>
2022-10-05 22:51:03 -0700

    retract command

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/IntakeRetractCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/IntakeRetractCommand.java
@@ -9,9 +9,10 @@ import org.firstinspires.ftc.teamcode.common.hardware.Robot;
 public class IntakeRetractCommand extends SequentialCommandGroup {
     public IntakeRetractCommand(Robot robot) {
         super(
-                new InstantCommand(() -> robot.intake.closeForebar()),
-                new InstantCommand(() -> robot.intake.extensionIn()),
-                new WaitUntilCommand(() -> robot.intake.getExtension() < robot.intake.extension_out_pos - 10)
+//                new InstantCommand(() -> robot.intake.closeForebar()),
+//                new InstantCommand(() -> robot.intake.extensionIn()),
+//                new WaitUntilCommand(() -> robot.intake.getExtension() < robot.intake.extension_out_pos - 10)
+                new IntakeCommand(robot.intake, robot.intake.intake_in_pos)
         );
 
     }
