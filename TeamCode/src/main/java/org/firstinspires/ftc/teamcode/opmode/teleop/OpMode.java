757743b474169fcaccc05685f48fecd63c76bbb7
Mason Stuart <mason.stuart05@outlook.com>
2022-10-07 10:15:46 -0700

    loop time counter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/OpMode.java
@@ -1,19 +1,30 @@
 package org.firstinspires.ftc.teamcode.opmode.teleop;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.common.hardware.Robot;
 
 public class OpMode extends CommandOpMode {
     private Robot robot;
 
+    private ElapsedTime time;
+    private double loopTime = 0;
+
     @Override
     public void initialize() {
         robot = new Robot(hardwareMap);
+
+        robot.intake.openClaw();
     }
 
     @Override
     public void run() {
+        double loopTime2 = System.currentTimeMillis();
 
+        telemetry.addData("u/s: ", loopTime2 - loopTime);
+        telemetry.update();
+
+        loopTime = System.currentTimeMillis();
     }
 }
