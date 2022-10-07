122e90fcd9350612faeb0ee2e0ea4f833368b619
Mason Stuart <mason.stuart05@outlook.com>
2022-10-06 23:59:21 -0700

    robot instantiation and abstract methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/OpMode.java
@@ -2,5 +2,13 @@ package org.firstinspires.ftc.teamcode.opmode.teleop;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
 
+import org.firstinspires.ftc.teamcode.common.hardware.Robot;
+
 public class OpMode extends CommandOpMode {
+    private Robot robot;
+
+    @Override
+    public void initialize() {
+        robot = new Robot(hardwareMap);
+    }
 }
