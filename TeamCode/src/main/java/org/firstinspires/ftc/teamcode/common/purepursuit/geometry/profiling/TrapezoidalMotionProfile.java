028dd46bf6573609ed8a7b1ec2b9e7e8e0548016
Mason Stuart <mason.stuart05@outlook.com>
2022-10-08 18:01:07 -0700

    trapezoidal motion profile vars setup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/purepursuit/geometry/profiling/TrapezoidalMotionProfile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/purepursuit/geometry/profiling/TrapezoidalMotionProfile.java
@@ -4,17 +4,21 @@ public class TrapezoidalMotionProfile implements MotionProfile {
     private final double maxV;
     private final double maxA;
     private final double distance;
+    private final double tNorm;
+    private final double tA;
 
     public TrapezoidalMotionProfile(double maxV, double maxA, double distance) {
         this.maxV = maxV;
         this.maxA = maxA;
         this.distance = distance;
+
+        this.tNorm = distance / maxV;
+        this.tA = maxV / maxA;
     }
 
     @Override
     public double update(double time)
     {
-        double velocity = 0.0;
-        return Math.min(velocity, maxV);
+        i
     }
 }
