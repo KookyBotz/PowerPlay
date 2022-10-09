a1566e06c9c9044b18d4b7f1fcef0274261782e6
Mason Stuart <mason.stuart05@outlook.com>
2022-10-09 12:25:33 -0700

    Added config tag and flipped array

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/purepursuit/geometry/Kinematics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/purepursuit/geometry/Kinematics.java
@@ -1,13 +1,16 @@
 package org.firstinspires.ftc.teamcode.common.purepursuit.geometry;
 
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
 public class Kinematics {
 
-    private final double FOREBAR_LENGTH = 9.842;
+    private static final double FOREBAR_LENGTH = 9.842;
 
     public double[] forebar(double x, double y) {
         double targetTheta = Math.sin(y / FOREBAR_LENGTH);
         double targetInches = Math.sqrt(Math.pow(y, 2) - Math.pow(FOREBAR_LENGTH, 2));
 
-        return new double[]{targetTheta, targetInches};
+        return new double[]{targetInches, targetTheta};
     }
 }
