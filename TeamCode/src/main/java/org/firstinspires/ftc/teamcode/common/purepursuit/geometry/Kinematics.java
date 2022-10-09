0b8a5483e08d0177309d47845ba9b13a70b449a6
Mason Stuart <mason.stuart05@outlook.com>
2022-10-09 13:55:53 -0700

    kinematics done for v4b

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/purepursuit/geometry/Kinematics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/purepursuit/geometry/Kinematics.java
@@ -7,7 +7,7 @@ public class Kinematics {
 
     private static final double FOREBAR_LENGTH = 9.842;
 
-    public double[] forebar(double x, double y) {
+    public static double[] forebar(double x, double y) {
         double targetInches = Math.sqrt(Math.pow(FOREBAR_LENGTH, 2) - Math.pow(y, 2));
         double targetRadians = Math.asin(y / FOREBAR_LENGTH);
 
