8803464e3f8a0cee45495a6b8a36008b2e1995fd
Mason Stuart <mason.stuart05@outlook.com>
2022-10-10 08:21:24 -0700

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/ExtensionCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/ExtensionCommand.java
@@ -3,26 +3,23 @@ package org.firstinspires.ftc.teamcode.common.commandbase.command.subsystemcomma
 import com.arcrobotics.ftclib.command.CommandBase;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 
+import org.firstinspires.ftc.teamcode.common.hardware.Robot;
 import org.firstinspires.ftc.teamcode.common.purepursuit.geometry.Kinematics;
 import org.firstinspires.ftc.teamcode.common.subsystem.IntakeSubsystem;
 import org.firstinspires.ftc.teamcode.opmode.test.System.ExtensionTest;
 
-public class ExtensionCommand extends CommandBase {
+public class ExtensionCommand extends SequentialCommandGroup {
     private IntakeSubsystem intake;
     private double[] points;
 
-    public ExtensionCommand(IntakeSubsystem intake, double x, double y) {
+    public ExtensionCommand(Robot robot, double x, double y) {
+        super(
+
+            new IntakeCommand(robot.intake, Kinematics.forebar(x, y, robot.intake.FOREBAR_LENGTH)[0])
+        );
         this.intake = intake;
         this.points = Kinematics.forebar(x, y, intake.FOREBAR_LENGTH);
-    }
 
-    @Override
-    public void execute() {
-
-    }
-
-    @Override
-    public boolean isFinished() {
 
     }
 }
