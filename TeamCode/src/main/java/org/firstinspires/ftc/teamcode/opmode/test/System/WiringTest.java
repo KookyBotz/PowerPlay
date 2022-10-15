ea6d64f630e7e21ae7a4235f6ea8c2382a62e990
Mason Stuart <mason.stuart05@outlook.com>
2022-10-14 22:40:04 -0700

    wiring test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/System/WiringTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/System/WiringTest.java
@@ -1,6 +1,37 @@
 package org.firstinspires.ftc.teamcode.opmode.test.System;
 
+import com.qualcomm.robotcore.hardware.Servo;
+
 import org.firstinspires.ftc.teamcode.opmode.teleop.OpMode;
 
 public class WiringTest extends OpMode {
+
+    Servo a, b, c, d;
+
+    @Override
+    public void initialize() {
+        a = hardwareMap.get(Servo.class, "port0");
+        b = hardwareMap.get(Servo.class, "port1");
+        c = hardwareMap.get(Servo.class, "port2");
+        d = hardwareMap.get(Servo.class, "port3");
+    }
+
+    @Override
+    public void run() {
+        if (gamepad1.a) {
+            a.setPosition(1);
+        }
+
+        if (gamepad1.b) {
+            b.setPosition(1);
+        }
+
+        if (gamepad1.x) {
+            c.setPosition(1);
+        }
+
+        if (gamepad1.y) {
+            d.setPosition(1);
+        }
+    }
 }
