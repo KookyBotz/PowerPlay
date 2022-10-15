1fd0902be8641a69ce8c91491447b530e416d833
Mason Stuart <mason.stuart05@outlook.com>
2022-10-15 08:05:29 -0700

    motor flipping

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/System/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/System/MotorTest.java
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode.opmode.test.System;
 
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.teamcode.opmode.teleop.OpMode;
 
+@TeleOp(name = "MotorConfigTest")
 public class MotorTest extends OpMode {
     // port 0 reverse set to -1
 
@@ -15,23 +17,26 @@ public class MotorTest extends OpMode {
         a = new MotorEx(hardwareMap, "extension");
         a.motor.setDirection(DcMotorSimple.Direction.REVERSE);
         b = new MotorEx(hardwareMap, "lift");
-//        c = new MotorEx(hardwareMap, "asd1");
-//        d = new MotorEx(hardwareMap, "asd2");
-//        e = new MotorEx(hardwareMap, "asd3");
-//        f = new MotorEx(hardwareMap, "asd4");
-//        g = new MotorEx(hardwareMap, "asd5");
-//        h = new MotorEx(hardwareMap, "asd6");
+        c = new MotorEx(hardwareMap, "asd1");
+        d = new MotorEx(hardwareMap, "asd2");
+        e = new MotorEx(hardwareMap, "asd3");
+        f = new MotorEx(hardwareMap, "asd4");
+        g = new MotorEx(hardwareMap, "asd5");
+        h = new MotorEx(hardwareMap, "asd6");
     }
 
     @Override
     public void run() {
-        a.set(-0.1);
-        b.set(0.1);
-//        c.set(0.1);
-//        d.set(0.1);
-//        e.set(0.1);
-//        f.set(0.1);
-//        g.set(0.1);
-//        h.set(0.1);
+//        if (gamepad1.a) {
+//
+//        }
+        a.set(0.25);
+        b.set(0.25);
+        c.set(0.25);
+        d.set(0.25);
+        e.set(0.25);
+        f.set(0.25);
+        g.set(0.25);
+        h.set(0.25);
     }
 }
