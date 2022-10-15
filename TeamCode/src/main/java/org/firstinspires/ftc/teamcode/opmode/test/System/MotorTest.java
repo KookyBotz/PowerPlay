2a1b24c595fcc772c2c83c36bb6d6ec7c070e2f9
Mason Stuart <mason.stuart05@outlook.com>
2022-10-15 07:54:12 -0700

    motor test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/System/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/System/MotorTest.java
@@ -14,24 +14,24 @@ public class MotorTest extends OpMode {
     public void initialize() {
         a = new MotorEx(hardwareMap, "extension");
         a.motor.setDirection(DcMotorSimple.Direction.REVERSE);
-        b = new MotorEx(hardwareMap, "asd");
-        c = new MotorEx(hardwareMap, "asd1");
-        d = new MotorEx(hardwareMap, "asd2");
-        e = new MotorEx(hardwareMap, "asd3");
-        f = new MotorEx(hardwareMap, "asd4");
-        g = new MotorEx(hardwareMap, "asd5");
-        h = new MotorEx(hardwareMap, "asd6");
+        b = new MotorEx(hardwareMap, "lift");
+//        c = new MotorEx(hardwareMap, "asd1");
+//        d = new MotorEx(hardwareMap, "asd2");
+//        e = new MotorEx(hardwareMap, "asd3");
+//        f = new MotorEx(hardwareMap, "asd4");
+//        g = new MotorEx(hardwareMap, "asd5");
+//        h = new MotorEx(hardwareMap, "asd6");
     }
 
     @Override
     public void run() {
         a.set(-0.1);
         b.set(0.1);
-        c.set(0.1);
-        d.set(0.1);
-        e.set(0.1);
-        f.set(0.1);
-        g.set(0.1);
-        h.set(0.1);
+//        c.set(0.1);
+//        d.set(0.1);
+//        e.set(0.1);
+//        f.set(0.1);
+//        g.set(0.1);
+//        h.set(0.1);
     }
 }
