51bac87b8a70e67cafa12ccc23fe51933a244890
Mason Stuart <mason.stuart05@outlook.com>
2022-10-15 13:30:54 -0700

    Fixed to make it parallel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/auto/CycleCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/auto/CycleCommand.java
@@ -8,27 +8,24 @@ import com.arcrobotics.ftclib.command.WaitUntilCommand;
 
 import org.firstinspires.ftc.teamcode.common.hardware.Robot;
 
-public class CycleCommand extends SequentialCommandGroup {
+public class CycleCommand extends ParallelCommandGroup {
     public CycleCommand(Robot robot) {
         super (
-            new ParallelCommandGroup(
-                new InstantCommand(() -> robot.intake.setPos(400)),
-                new InstantCommand(() -> robot.intake.openClaw()),
-                new InstantCommand(() -> robot.intake.extendForebar()),
-                new SequentialCommandGroup(
-                    new WaitUntilCommand(()->robot.intake.getPos() > 390),
-                    new WaitCommand(1000),
-                    new InstantCommand(() -> robot.intake.closeClaw()),
-                    new WaitCommand(250),
-                    new InstantCommand(() -> robot.intake.closeForebar()),
-                    new InstantCommand(() -> robot.intake.setPos(-400))
-                ),
-                new SequentialCommandGroup(
-                    new InstantCommand(() -> robot.lift.setPos(500)),
-                    new WaitCommand(500),
-                    new InstantCommand(() -> robot.lift.setPos(-500))
-                )
-
+            new InstantCommand(() -> robot.intake.setPos(400)),
+            new InstantCommand(() -> robot.intake.openClaw()),
+            new InstantCommand(() -> robot.intake.extendForebar()),
+            new SequentialCommandGroup(
+                new WaitUntilCommand(()->robot.intake.getPos() > 390),
+                new WaitCommand(1000),
+                new InstantCommand(() -> robot.intake.closeClaw()),
+                new WaitCommand(250),
+                new InstantCommand(() -> robot.intake.closeForebar()),
+                new InstantCommand(() -> robot.intake.setPos(-400))
+            ),
+            new SequentialCommandGroup(
+                new InstantCommand(() -> robot.lift.setPos(500)),
+                new WaitCommand(500),
+                new InstantCommand(() -> robot.lift.setPos(-500))
             )
         );
     }
