b4e7cebdafa5a1efc6cd811bbf0c87a580f22d84
Mason Stuart <mason.stuart05@outlook.com>
2022-10-17 20:40:44 -0700

    lift command changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/auto/LiftCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/auto/LiftCommand.java
@@ -1,18 +1,35 @@
 package org.firstinspires.ftc.teamcode.common.commandbase.command.subsystemcommands.auto;
 
+import com.arcrobotics.ftclib.command.CommandBase;
 import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 
 import org.firstinspires.ftc.teamcode.common.hardware.Robot;
+import org.firstinspires.ftc.teamcode.common.subsystem.LiftSubsystem;
 
-public class LiftCommand extends SequentialCommandGroup {
-    public LiftCommand(Robot robot, int distance, int maxV, int maxA) {
+public class LiftCommand extends CommandBase {
+    LiftSubsystem lift;
+    private int distance;
+    private int maxV;
+    private int maxA;
+
+    public LiftCommand(LiftSubsystem lift, int distance, int maxV, int maxA) {
+        this.lift = lift;
         if (distance == 0) {
-            distance = robot.intake.getPos();
+            distance = -lift.getPos();
+            maxV *= -1;
+            maxA *= -1;
         }
-        super(
-            new InstantCommand(() -> robot.lift.resetTimer()),
-            new InstantCommand(() -> robot.lift.setDVA())
-        );
+    }
+
+    @Override
+    public void execute() {
+        lift.setDVA(distance, maxV, maxA);
+        lift.resetTimer();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return super.isFinished();
     }
 }
