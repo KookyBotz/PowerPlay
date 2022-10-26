2e40817e5efb0cd642c08c8299cc77f29b3c962d
Mason Stuart <mason.stuart05@outlook.com>
2022-10-26 09:20:15 -0700

    we do some tomfoolery

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/powerplay/CMYSleeveDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/powerplay/CMYSleeveDetection.java
@@ -11,16 +11,30 @@ public class CMYSleeveDetection extends OpenCvPipeline {
 
     @Override
     public Mat processFrame(Mat input) {
-        Mat newMat = new Mat();
-        newMat.convertTo(newMat, CvType.CV_16U);
+//        Mat newMat = new Mat();
+//        newMat.convertTo(input, CvType.CV_16U);
+//        for (int i = 0; i < input.height(); i++) {
+//            for (int j = 0; j < input.width(); j++) {
+////                double[] colors = input.get(j, i);
+////                double k = Math.max(colors[0], Math.max(colors[1], colors[2]));
+////                double cyan = (1 - colors[0] - k) / (1 - k);
+//                input.put(j, i);
+//            }
+//        }
         for (int i = 0; i < input.height(); i++) {
             for (int j = 0; j < input.width(); j++) {
-                double[] channels = input.get(j, i);
-                double k = Math.max(channels[0], Math.max(channels[1], channels[2]));
-                double cyan = (1 - channels[0] - k) / (1 - k);
-                Imgproc.circle(newMat, new Point(j, i), 0, new Scalar(cyan, cyan, cyan));
+                double[] colors = input.get(j, i);
+                try {
+                    double k = Math.max(colors[0], Math.max(colors[1], colors[2]));
+                    int cyan = (int)((1 - colors[0] - k) / (1 - k));
+                    input.put(i, j, 255, 255, 255, 0);
+                } catch (Exception e) {
+                    input.put(i, j, 255, 255, 255, 255);
+                }
+
+
             }
         }
-        return newMat;
+        return input;
     }
 }
