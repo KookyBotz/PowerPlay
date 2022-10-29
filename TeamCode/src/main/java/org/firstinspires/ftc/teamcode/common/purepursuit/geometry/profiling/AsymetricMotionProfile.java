014f0805f4472f3652687e512cec38623efdf7f2
Mason Stuart <mason.stuart05@outlook.com>
2022-10-29 13:50:00 -0700

    finished asymmetric motion profiling

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/purepursuit/geometry/profiling/AsymetricMotionProfile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/purepursuit/geometry/profiling/AsymetricMotionProfile.java
@@ -31,10 +31,52 @@ public class AsymetricMotionProfile {
         double averageDt = (this.dt1 + this.dt3) / 2;
         this.dt2 = Math.abs(distance) / Math.abs(constraints.max_velocity) - averageDt;
 
+        if (this.dt2 < 0) {
+            this.dt2 = 0;
 
+            if (Math.abs(this.constraints.max_acceleration) > Math.abs(constraints.max_deceleration)) {
+                constraints.max_acceleration = Math.abs(Math.abs(constraints.max_deceleration));
+            } else {
+                constraints.max_deceleration = Math.abs(Math.abs(constraints.max_acceleration));
+            }
+
+            this.dt1 = Math.sqrt(Math.abs(distance) / Math.abs(constraints.max_acceleration));
+            this.dt3 = Math.sqrt(Math.abs(distance) / Math.abs(constraints.max_deceleration));
+        }
+
+        this.profileDuration = this.dt1 + this.dt2 + this.dt3;
     }
 
     public MotionState calculate(double seconds) {
-        return null;
+        double acceleration;
+        double velocity;
+        double position;
+
+        if (seconds <= this.dt1) {
+            acceleration = Math.abs(constraints.max_acceleration);
+            velocity = seconds * acceleration;
+
+            position = 0.5 * acceleration * Math.pow(seconds, 2);
+        } else if (seconds <= this.dt1 + this.dt2) {
+            acceleration = 0;
+            velocity = Math.abs(calculate(this.dt1).v);
+
+            position = Math.abs(calculate(this.dt1).x) + constraints.max_velocity * (seconds - this.dt1);
+        } else if (seconds <= this.dt1 + this.dt2 + this.dt3) {
+            acceleration = Math.abs(constraints.max_deceleration);
+            double coastVelocity = Math.abs(calculate(this.dt1).v);
+            velocity = coastVelocity - (seconds - this.dt1 - this.dt2) * acceleration;
+
+            double endofdt2 = this.dt1 + this.dt2;
+            double endOfdt2Pos = Math.abs(calculate(endofdt2).x);
+            position = endOfdt2Pos + coastVelocity * (seconds - endofdt2) - 0.5 * acceleration * Math.pow(seconds - endofdt2, 2);
+            acceleration *= -1;
+        } else {
+            acceleration = 0;
+            velocity = 0;
+            return new MotionState(finalPosition, velocity * Math.signum(distance), acceleration * Math.signum(distance));
+        }
+
+        return new MotionState(this.initialPosition + position * Math.signum(distance), velocity * Math.signum(distance), acceleration * Math.signum(distance));
     }
 }
