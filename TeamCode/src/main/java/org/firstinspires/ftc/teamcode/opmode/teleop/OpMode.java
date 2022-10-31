442247b8e75fcfc3fdf183054f7357af6ec9b7fb
Mason Stuart <mason.stuart05@outlook.com>
2022-10-31 00:31:53 -0700

    refactor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/OpMode.java
@@ -5,18 +5,12 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.CommandScheduler;
-import com.arcrobotics.ftclib.command.InstantCommand;
-import com.arcrobotics.ftclib.command.SequentialCommandGroup;
-import com.arcrobotics.ftclib.command.WaitCommand;
 import com.outoftheboxrobotics.photoncore.PhotonCore;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
-import org.firstinspires.ftc.teamcode.common.commandbase.command.subsystemcommands.IntakeExtendCommand;
-import org.firstinspires.ftc.teamcode.common.commandbase.command.subsystemcommands.auto.CycleCommand;
 import org.firstinspires.ftc.teamcode.common.hardware.Robot;
 import org.firstinspires.ftc.teamcode.common.purepursuit.geometry.Point;
 import org.firstinspires.ftc.teamcode.common.purepursuit.geometry.Pose;
@@ -28,7 +22,6 @@ public class OpMode extends CommandOpMode {
 
     private ElapsedTime timer;
     private double loopTime = 0;
-    private boolean fRB = false;
 
     public static double extensionPower = 0.1;
     public static double liftPower = 0.1;
