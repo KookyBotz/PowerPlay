02197f6636eda40b85a5aa367fe67faff0542080
Mason Stuart <mason.stuart05@outlook.com>
2022-11-10 13:05:42 -0800

    small more bits of refactoring

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/IntakeExtendCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/IntakeExtendCommand.java
@@ -6,15 +6,15 @@ import org.firstinspires.ftc.teamcode.common.hardware.Robot;
 import org.firstinspires.ftc.teamcode.common.purepursuit.geometry.Kinematics;
 
 public class IntakeExtendCommand extends SequentialCommandGroup {
-    public IntakeExtendCommand(Robot robot) {
-        super(
+//    public IntakeExtendCommand(Robot robot) {
+//        super(
 //                double[] arr = Kinematics.forebar(x, y);
 //                new WaitUntilCommand(() -> robot.lift.getPos() < robot.lift.high_pos - 50),
 //                new InstantCommand(() -> robot.intake.extensionOut())
 //                .alongWith(new InstantCommand(() -> robot.intake.openClaw()))
 //                .alongWith(new InstantCommand(() -> robot.intake.extendForebar()))
-                new IntakeCommand(robot.intake, robot.intake.intake_out_pos)
+//                new IntakeCommand(robot.intake, robot.intake.intake_out_pos)
                 //.alongWith(new ForebarCommand(robot.intake, ))
-        );
-    }
+//        );
+//    }
 }
