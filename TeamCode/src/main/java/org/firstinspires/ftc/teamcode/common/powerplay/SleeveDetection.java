7a1a39908e29553545109d74f56f237eb86771a3
Mason Stuart <mason.stuart05@outlook.com>
2022-11-11 11:35:14 -0800

    Fully optimized vision detection code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/powerplay/SleeveDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/powerplay/SleeveDetection.java
@@ -38,9 +38,6 @@ public class SleeveDetection extends OpenCvPipeline {
             CYAN    = new Scalar(0, 255, 255),
             MAGENTA = new Scalar(255, 0, 255);
 
-    // Percent and mat definitions
-    private Mat yelMat, cyaMat, magMat, blurredMat, kernel;
-
     // Anchor point definitions
     Point sleeve_pointA = new Point(
             SLEEVE_TOPLEFT_ANCHOR_POINT.x,
@@ -66,70 +63,7 @@ public class SleeveDetection extends OpenCvPipeline {
     public SleeveDetection() {}
 
     @Override
-    public void init(Mat input) {
-        yelMat = new Mat();
-        cyaMat = new Mat();
-        magMat = new Mat();
-        blurredMat = new Mat();
-        kernel = new Mat();
-    }
-
-    @Override
     public Mat processFrame(Mat input) {
-//        // Noise reduction
-//        Imgproc.blur(input, blurredMat, new Size(5, 5));
-//        blurredMat = blurredMat.submat(new Rect(sleeve_pointA, sleeve_pointB));
-//
-//        // Apply Morphology
-//        kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
-//        Imgproc.morphologyEx(blurredMat, blurredMat, Imgproc.MORPH_CLOSE, kernel);
-//
-//        // Gets channels from given source mat
-//        Core.inRange(blurredMat, lower_yellow_bounds, upper_yellow_bounds, yelMat);
-//        Core.inRange(blurredMat, lower_cyan_bounds, upper_cyan_bounds, cyaMat);
-//        Core.inRange(blurredMat, lower_magenta_bounds, upper_magenta_bounds, magMat);
-//
-//        // Gets color specific values
-//        yelPercent = Core.countNonZero(yelMat);
-//        cyaPercent = Core.countNonZero(cyaMat);
-//        magPercent = Core.countNonZero(magMat);
-//
-//
-//        // Calculates the highest amount of pixels being covered on each side
-//        double maxPercent = Math.max(yelPercent, Math.max(cyaPercent, magPercent));
-//
-//        // Checks all percentages, will highlight bounding box in camera preview
-//        // based on what color is being detected
-//        if (maxPercent == yelPercent) {
-//            position = ParkingPosition.LEFT;
-//            Imgproc.rectangle(
-//                    input,
-//                    sleeve_pointA,
-//                    sleeve_pointB,
-//                    YELLOW,
-//                    2
-//            );
-//        } else if (maxPercent == cyaPercent) {
-//            position = ParkingPosition.CENTER;
-//            Imgproc.rectangle(
-//                    input,
-//                    sleeve_pointA,
-//                    sleeve_pointB,
-//                    CYAN,
-//                    2
-//            );
-//        } else if (maxPercent == magPercent) {
-//            position = ParkingPosition.RIGHT;
-//            Imgproc.rectangle(
-//                    input,
-//                    sleeve_pointA,
-//                    sleeve_pointB,
-//                    MAGENTA,
-//                    2
-//            );
-//        }
-//        Imgproc.circle(input, new Point(SLEEVE_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH / 2, SLEEVE_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT / 2), 1, new Scalar(255, 255, 255));
-//
         Mat colorMat = input.submat(new Rect(sleeve_pointA, sleeve_pointB));
         Scalar sumColors = Core.sumElems(colorMat);
 
@@ -165,13 +99,7 @@ public class SleeveDetection extends OpenCvPipeline {
             );
         }
 
-        // Memory cleanup
-        blurredMat.release();
-        yelMat.release();
-        cyaMat.release();
-        magMat.release();
-        kernel.release();
-
+        colorMat.release();
         return input;
     }
 
