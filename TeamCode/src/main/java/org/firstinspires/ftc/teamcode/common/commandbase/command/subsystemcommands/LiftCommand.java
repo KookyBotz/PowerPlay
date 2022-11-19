2a1ae3e8a31671ac8ea654b766c73eceffe717e6
Mason Stuart <mason.stuart05@outlook.com>
2022-11-19 10:48:57 -0800

    omg more fixes REUHJORIUAE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/LiftCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/LiftCommand.java
@@ -6,18 +6,20 @@ import com.arcrobotics.ftclib.command.WaitCommand;
 import com.arcrobotics.ftclib.command.WaitUntilCommand;
 
 import org.firstinspires.ftc.teamcode.common.commandbase.command.subsystemcommands.subsystem.ClawCommand;
+import org.firstinspires.ftc.teamcode.common.commandbase.command.subsystemcommands.subsystem.LatchCommand;
 import org.firstinspires.ftc.teamcode.common.commandbase.command.subsystemcommands.subsystem.TurretCommand;
 import org.firstinspires.ftc.teamcode.common.hardware.Robot;
 import org.firstinspires.ftc.teamcode.common.subsystem.IntakeSubsystem;
+import org.firstinspires.ftc.teamcode.common.subsystem.LiftSubsystem;
 
 public class LiftCommand extends SequentialCommandGroup {
     public LiftCommand(Robot robot, double targetPos, double velocity, double acceleration) {
         super(
                 new ClawCommand(robot, IntakeSubsystem.ClawState.OPEN),
                 new WaitCommand(250),
+                new LatchCommand(robot, LiftSubsystem.LatchState.LATCHED),
                 new ClearFourbarCommand(robot.intake),
                 new WaitUntilCommand(() -> robot.intake.getFourbarPos() <= robot.intake.fourbar_transition),
-
                 new WaitCommand(250),
                 new InstantCommand(() -> robot.lift.newProfile(targetPos, velocity, acceleration)),
                 new TurretCommand(robot, IntakeSubsystem.TurretState.INTAKE)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/subsystem/LatchCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/command/subsystemcommands/subsystem/LatchCommand.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.common.commandbase.command.subsystemcomma
 import com.arcrobotics.ftclib.command.InstantCommand;
 
 import org.firstinspires.ftc.teamcode.common.hardware.Robot;
-import org.firstinspires.ftc.teamcode.common.subsystem.IntakeSubsystem;
 import org.firstinspires.ftc.teamcode.common.subsystem.LiftSubsystem;
 
 public class LatchCommand extends InstantCommand {
