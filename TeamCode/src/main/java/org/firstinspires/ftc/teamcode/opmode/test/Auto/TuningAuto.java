fb7bf2b0b70fe734935fe9b55b76936654654fbe
Mason Stuart <mason.stuart05@outlook.com>
2022-11-24 17:06:46 -0800

    tuning auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/Auto/TuningAuto.java
@@ -0,0 +1,91 @@
+package org.firstinspires.ftc.teamcode.opmode.test.Auto;
+
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+import com.outoftheboxrobotics.photoncore.PhotonCore;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.common.commandbase.command.subsystemcommands.PositionCommand;
+import org.firstinspires.ftc.teamcode.common.hardware.Robot;
+import org.firstinspires.ftc.teamcode.common.powerplay.SleeveDetection;
+import org.firstinspires.ftc.teamcode.common.purepursuit.drive.Drivetrain;
+import org.firstinspires.ftc.teamcode.common.purepursuit.drive.swerve.SwerveModule;
+import org.firstinspires.ftc.teamcode.common.purepursuit.geometry.Pose;
+import org.firstinspires.ftc.teamcode.common.purepursuit.localizer.Localizer;
+import org.firstinspires.ftc.teamcode.common.purepursuit.localizer.TwoWheelLocalizer;
+import org.firstinspires.ftc.teamcode.common.subsystem.IntakeSubsystem;
+import org.firstinspires.ftc.teamcode.common.subsystem.LiftSubsystem;
+import org.opencv.core.Point;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+@Autonomous(name = "LeftAuto")
+public class TuningAuto extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        CommandScheduler.getInstance().reset();
+        Robot robot = new Robot(hardwareMap, true);
+        Drivetrain drivetrain = robot.drivetrain;
+
+        Localizer localizer = new TwoWheelLocalizer(
+                () -> robot.horizontalEncoder.getPosition(),
+                () -> robot.lateralEncoder.getPosition(),
+                robot::getAngle
+        );
+        robot.localizer = localizer;
+        robot.intake.update(IntakeSubsystem.FourbarState.DEPOSIT);
+        robot.lift.update(LiftSubsystem.LatchState.UNLATCHED);
+
+        PhotonCore.CONTROL_HUB.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
+        PhotonCore.EXPANSION_HUB.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
+        PhotonCore.experimental.setMaximumParallelCommands(8);
+        PhotonCore.enable();
+
+        while (!isStarted()) {
+            robot.read();
+            for (SwerveModule module : robot.drivetrain.modules) {
+                module.setTargetRotation(0);
+            }
+            robot.drivetrain.updateModules();
+
+            telemetry.addLine("RUNNING LEFT 5 CYCLE");
+            telemetry.update();
+
+            PhotonCore.CONTROL_HUB.clearBulkCache();
+            PhotonCore.EXPANSION_HUB.clearBulkCache();
+            robot.write();
+        }
+
+
+        waitForStart();
+
+        CommandScheduler.getInstance().schedule(
+                new SequentialCommandGroup(
+                        new PositionCommand(drivetrain, localizer, new Pose(0, 10, 0), 1750)
+
+                )
+        );
+
+        while (opModeIsActive()) {
+            robot.read();
+            CommandScheduler.getInstance().run();
+            robot.intake.loop();
+            robot.lift.loop();
+            robot.drivetrain.updateModules();
+            localizer.periodic();
+            telemetry.addData("current pose", localizer.getPos());
+            telemetry.update();
+            robot.write();
+            PhotonCore.CONTROL_HUB.clearBulkCache();
+            PhotonCore.EXPANSION_HUB.clearBulkCache();
+        }
+    }
+}
