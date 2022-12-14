c0e965e6bf39df18289eb734446e7d1d906adc7f
veernahar <43395575+veernahar@users.noreply.github.com>
2022-12-13 19:18:18 -0800

    auto e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/auto/AutoCycleCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/auto/AutoCycleCommand.java
@@ -38,8 +38,8 @@ public class AutoCycleCommand extends SequentialCommandGroup {
                                 new WaitCommand(250),
                                 // transfer position
                                 new InstantCommand(() -> robot.intake.update(IntakeSubsystem.FourbarState.TRANSITION)),
-                                new InstantCommand(() -> robot.intake.update(IntakeSubsystem.TurretState.DEPOSIT)),
-                                new IntakePositionCommand(robot.intake, 15, 3000, 3000, 10, 3000, IntakeSubsystem.STATE.FAILED_RETRACT),
+                                new IntakePositionCommand(robot.intake, 15, 3000, 3000, 10, 3000, IntakeSubsystem.STATE.FAILED_RETRACT)
+                                        .alongWith(new WaitCommand(500).andThen(new InstantCommand(() -> robot.intake.update(IntakeSubsystem.TurretState.DEPOSIT)))),
                                 new InstantCommand(() -> robot.intake.update(IntakeSubsystem.FourbarState.DEPOSIT)),
 
 
