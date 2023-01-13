e8f1d901de6a630aaf998b50f1c3f5e1fef0df78
Mason Stuart <mason.stuart05@outlook.com>
2023-01-12 16:01:26 -0800

    intake/transfer teleop changed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/auto/TeleopIntakeCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/auto/TeleopIntakeCommand.java
@@ -2,31 +2,16 @@ package org.firstinspires.ftc.teamcode.common.commandbase.auto;
 
 import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
-import com.arcrobotics.ftclib.command.WaitCommand;
-import com.arcrobotics.ftclib.command.WaitUntilCommand;
 
-import org.firstinspires.ftc.teamcode.common.commandbase.commands.IntakePositionCommand;
 import org.firstinspires.ftc.teamcode.common.commandbase.subsystem.IntakeSubsystem;
 import org.firstinspires.ftc.teamcode.common.hardware.Robot;
 
 public class TeleopIntakeCommand extends SequentialCommandGroup {
     public TeleopIntakeCommand(Robot robot) {
         super(
-                new WaitUntilCommand(() -> robot.lift.getTargetPos() < 20 && robot.lift.getPos() < 563),
-                new InstantCommand(() -> robot.intake.update(IntakeSubsystem.ClawState.CLOSED)),
-                new WaitCommand(150),
-                new InstantCommand(() -> robot.intake.update(IntakeSubsystem.FourbarState.TRANSITION)),
-
-                new InstantCommand(() -> robot.intake.setPivot(IntakeSubsystem.pivot_auto_transfer)),
-                new InstantCommand(() -> robot.intake.update(IntakeSubsystem.TurretState.DEPOSIT)),
-                new IntakePositionCommand(robot.intake, -5, 6000, 2500, 20, 3000, IntakeSubsystem.STATE.FAILED_RETRACT),
-
-
-                new InstantCommand(() -> robot.intake.update(IntakeSubsystem.FourbarState.DEPOSIT)),
-                new WaitCommand(50),
-                new InstantCommand(() -> robot.intake.setPivot(IntakeSubsystem.pivot_flat)),
-                new WaitCommand(250),
-                new InstantCommand(() -> robot.intake.update(IntakeSubsystem.ClawState.OPEN))
+                new InstantCommand(() -> robot.intake.update(IntakeSubsystem.ClawState.OPEN)),
+                new InstantCommand(() -> robot.intake.setFourbar(IntakeSubsystem.fourbar_extended)),
+                new InstantCommand(() -> robot.intake.update(IntakeSubsystem.PivotState.FLAT))
         );
     }
 }
