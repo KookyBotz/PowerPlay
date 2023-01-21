d1885b573f069b052dd483621c5d142d37b8624f
Mason Stuart <mason.stuart05@outlook.com>
2023-01-20 22:13:05 -0800

    veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey veer u monkey

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/subsystem/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/subsystem/ServoTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opmode.teleop;
+package org.firstinspires.ftc.teamcode.opmode.test.subsystem;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
